785d30bfb8a34563a6e50bf23f929f8cd3cb43bf
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-07-03 09:52:17 +0000

    Theme4 fixed

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -9,7 +9,7 @@ before_action :authenticate_user!
 
   # フォロー外すとき
   def destroy
-    user = User.find([:user_id])
+    user = User.find(params[:user_id])
     current_user.unfollow(user)
     redirect_to request.referer
   end
