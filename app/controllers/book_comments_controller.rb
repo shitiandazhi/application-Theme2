85a564b6cb74dd8d7aa93187a26a4d4fca265907
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-07-06 09:57:41 +0000

    Theme6 finish

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,26 +1,23 @@
 class BookCommentsController < ApplicationController
-
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    @book = Book.find(params[:book_id])
-    @book_comment = BookComment.new
-    #redirect_to book_path(book)
+   @book = Book.find(params[:book_id])
+   comment = current_user.book_comments.new(book_comment_params)
+   comment.book_id = @book.id
+   comment.save
+
+   @book_comment = BookComment.new
+
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
-    @book_comment = BookComment.new
-    #redirect_to book_path(params[:book_id])
-  end
+   BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+   @book = Book.find(params[:book_id])
 
-  private
+   @book_comment = BookComment.new
+  end
 
   def book_comment_params
     params.require(:book_comment).permit(:comment)
-  end
 
+  end
 end
