ebe0ae39474aa8642923f64f65a0cad0b1021606
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-07-06 07:58:42 +0000

    Theme6 add

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,16 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
+    #redirect_to book_path(book)
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book_comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @book_comment = BookComment.new
+    #redirect_to book_path(params[:book_id])
   end
 
   private
