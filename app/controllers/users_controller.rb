6ebfdb954ae966ee982edcc34b12f1700ef1381d
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-07-07 11:03:21 +0000

    Theme7 finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @bookss = @user.books
+    @books = @user.books
     @book = Book.new
-    @books = @user.books.page(params[:page]).reverse_order
     @today_book =  @books.created_today
     @yesterday_book = @books.created_yesterday
     @this_week_book = @books.created_this_week
